e351983fbfbb5b0c5fa56fb6d709e451aeb32749
Mikkel Malmberg <mikkel@brnbw.com>
2026-01-26 13:15:16 +0100

    Add examples/

---
+++ examples/input.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const opwait = require('pear-opwait')
+const { Interact, print } = require('..')
+
+async function main() {
+  const fields = {}
+  const interact = new Interact('Simple input example\n\n', [
+    {
+      name: 'name',
+      prompt: 'Name',
+      delim: ':',
+      default: 'Anonymous'
+    },
+    {
+      name: 'email',
+      prompt: 'Email',
+      delim: ':',
+      validation: (value) => value.includes('@'),
+      msg: 'Please enter a valid email.\n'
+    }
+  ])
+
+  await opwait(interact.run(), ({ tag, data }) => {
+    if (tag === 'input') fields[data.name] = data.answer
+  })
+
+  print(`\nThanks, ${fields.name}!`)
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exitCode = 1
+})
